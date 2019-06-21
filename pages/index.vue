c36b1993929c7488aa5ee393f9b30183b358262d
ChefyHase <ChefyHase@example.com>
2019-06-21 21:08:12 +0900

    update for #3

--- pages/index.vue
+++ pages/index.vue
@@ -10,15 +10,16 @@ section.hero.is-fullheight
             a(@click="toggleModal").navbar-item API
   div(@dragover.prevent @dragleave.prevent @drop.prevent="drop").hero-body
     .container
-      p.has-text-centered.is-size-1 {{ $store.state.fileName }}
-      progress(v-if="$store.state.isProgress").progress.is-danger.is-large
-      p(v-else).has-text-centered.is-size-1 {{ $store.state.res }}
+      template(v-for="(name, index) in $store.state.fileName")
+        p.has-text-centered.is-size-1 {{ name }}
+        progress(v-if="$store.state.isProgress").progress.is-danger.is-large
+        p(v-else).has-text-centered.is-size-1 {{ $store.state.res[index] }}
 
       div(v-if="!$store.state.fileSetted").file.is-boxed
         label.file-label.is-center
-          input(type="file" name="file" @change="showFileName")#file.file-input
+          input(type="file" name="file" @change="showFileName" multiple)#file.file-input
           span.file-cta
-            span.file-label Choose a file or Drug & Drop
+            span.file-label Choose files or Drug & Drop
 
   div#modal.modal
     .modal-background
@@ -36,32 +37,36 @@ import axios from 'axios'
 export default {
   methods: {
     showFileName: function() {
-      const fileSelectDom = document.getElementById('file').files[0]
+      const fileSelectDom = document.getElementById('file').files
       this.$store.commit('updateFileName', fileSelectDom)
       this.$store.commit('fileSetToggle')
 
       this.upload()
     },
     upload: async function() {
-      const fileSelectDom = this.$store.getters.getFiledom
-      const params = new FormData()
+      const fileSelectDoms = this.$store.getters.getFiledom
+      const msgs = []
 
-      if (!fileSelectDom || fileSelectDom.type !== 'audio/wav') {
-        this.$store.commit('upload', 'invaild')
-        this.$store.commit('fileSetToggle')
-      } else {
-        this.$store.commit('toggleProgress')
+      for (const fileSelectDom of fileSelectDoms) {
+        const params = new FormData()
+        if (!fileSelectDom || fileSelectDom.type !== 'audio/wav') {
+          this.$store.commit('upload', 'invaild')
+          this.$store.commit('fileSetToggle')
+        } else {
+          this.$store.commit('toggleProgress')
 
-        params.append('file', fileSelectDom)
-        const res = await axios.post(
-          process.env.HOST + '/api/genshiChouritsu',
-          params
-        )
+          params.append('file', fileSelectDom)
+          const res = await axios.post(
+            process.env.HOST + '/api/genshiChouritsu',
+            params
+          )
 
-        this.$store.commit('toggleProgress')
-        this.$store.commit('upload', res.data.msg)
-        this.$store.commit('fileSetToggle')
+          this.$store.commit('toggleProgress')
+          msgs.push(res.data.msg)
+        }
       }
+      this.$store.commit('upload', msgs)
+      this.$store.commit('fileSetToggle')
     },
     toggleModal: function() {
       const modal = document.getElementById('modal')
